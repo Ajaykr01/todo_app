d1e6f0bd18eec83abf998a3d764d59a8b8ec2e69
Ajay <ajaykumar87577@gmail.com>
2024-09-25 10:49:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="relative top-32">
+      <div className="bg-black h-96 w-2/4 m-auto">
+        <h1 className="text-white text-5xl flex justify-center">To Do App</h1>
+      </div>
+    </div>
+  );
+}
+export default App;
