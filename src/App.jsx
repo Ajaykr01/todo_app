938c44c9402900bb8d979e0642fb4888e718d295
Ajay <ajaykumar87577@gmail.com>
2024-10-02 11:26:16 +0530

    added edit and delete btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,46 @@
 import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [inputList, setInputList] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const [editIndex, setEditIndex] = useState(null); // Track the index of the todo being edited
+  const [editValue, setEditValue] = useState(""); // Track the new value for the todo
 
-  const addtodo = () => {
-    // if (inputList == "") {
-    //   alert("plz add some todo");
-    // } else {
-    //   console.log("list submitted successfully",inputList);
-    // }
-    setInputList([...inputList, inputValue]); // Add new item to the list.
-    setInputValue("");
+  const addTodo = () => {
+    if (inputValue.trim() === "") {
+      alert("Please add some todo");
+    } else {
+      setInputList([...inputList, inputValue]);
+      setInputValue(""); // Clear the input field
+    }
+  };
+
+  const deleteTask = (index) => {
+    const newInputList = inputList.filter((_, i) => i !== index);
+    setInputList(newInputList); // Update state with the new list
+  };
+
+  const startEditing = (index) => {
+    setEditIndex(index); // Set the index of the todo to edit
+    setEditValue(inputList[index]); // Set the edit value to the current todo text
+  };
+
+  const saveEdit = () => {
+    const updatedList = inputList.map((item, i) =>
+      i === editIndex ? editValue : item
+    );
+    setInputList(updatedList); // Update the todo list with the edited item
+    setEditIndex(null); // Reset the editing state
+    setEditValue(""); // Clear the edit value
   };
 
   return (
     <div className="bg-black h-screen flex flex-col items-center">
       <h1 className="text-white text-5xl flex justify-center pt-5">
-        <i class="fa-solid fa-list-check pr-3 pt-1"></i>TODO
+        <i className="fa-solid fa-list-check pr-3 pt-1"></i>TODO
       </h1>
-      <div className="w-[40rem] h-[35rem] rounded-md left-1/4 border mt-10 overflow-y-scroll">
+      <div className="w-[40rem] h-[35rem] rounded-md left-1/4 border mt-10 overflow-y-scroll custom-scroll">
         <div className="flex justify-center pt-6 ml-5 mr-5 gap-5 sticky top-0 backdrop-blur-sm">
           <input
             type="text"
@@ -30,22 +51,50 @@ function App() {
           />
           <button
             className="bg-green-500 text-white min-w-16 text-3xl flex justify-center rounded-md"
-            onClick={addtodo}
+            onClick={addTodo}
           >
             +
           </button>
         </div>
         {inputList.map((item, index) => (
-          <div className="flex justify-around items-center h-12 border mt-4 ml-10 mr-10">
-            <div key={index} className="flex text-lg h-8 w-[25rem] ml-2">
-              <h1 className="text-white">{item}</h1>
+          <div
+            key={index}
+            className="flex justify-around items-center h-12 border mt-4 ml-10 mr-10"
+          >
+            <div className="flex text-lg w-[25rem] ml-2">
+              {editIndex === index ? (
+                // If we are editing this item, show an input field
+                <input
+                  type="text"
+                  value={editValue}
+                  onChange={(e) => setEditValue(e.target.value)}
+                  className="w-[25rem] bg-white text-black outline-none rounded-sm"
+                />
+              ) : (
+                <h1 className="text-white">{item}</h1>
+              )}
             </div>
             <div className="w-[7rem] flex justify-evenly">
-              <button className="h-10 w-12 text-2xl text-white">
-                <i class="fa-solid fa-pen-to-square"></i>
-              </button>
-              <button className="h-10 w-12 text-2xl text-red-500">
-                <i class="fa-solid fa-trash"></i>
+              {editIndex === index ? (
+                <button
+                  className="h-10 w-12 text-2xl text-white"
+                  onClick={saveEdit} // Call saveEdit to update the todo
+                >
+                  <i className="fa-solid fa-check"></i>
+                </button>
+              ) : (
+                <button
+                  className="h-10 w-12 text-2xl text-white"
+                  onClick={() => startEditing(index)}
+                >
+                  <i className="fa-solid fa-pen-to-square"></i>
+                </button>
+              )}
+              <button
+                className="h-10 w-12 text-2xl text-red-500"
+                onClick={() => deleteTask(index)} // Call deleteTask with the current index
+              >
+                <i className="fa-solid fa-trash"></i>
               </button>
             </div>
           </div>
@@ -54,4 +103,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
